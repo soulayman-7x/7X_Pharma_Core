614278ec6023647a581b95934d01f7a1ceaa86a7
soulayman-7x <idbahasevenx@gmail.com>
2026-05-13 13:26:42 +0100

    refactor: Remove financial constants related to the Moroccan market from configuration

--- app/config/Constants.php
+++ app/config/Constants.php
@@ -3,13 +3,7 @@
 define('BASE_URL', $_ENV['APP_URL'] ?? 'http://localhost/7X_Pharma_Core/public');
 define('ROOT_DIR', dirname(__DIR__, 2));
 
-// 2. Financial constants of the Moroccan market
-define('CURRENCY', 'MAD');
-define('TVA_MEDICINE', 0.07); // 7% = الضريبة المغربية على الادوية 
-define('TVA_PARA', 0.20); // 20% = الضريبة المغربية على مواد التجميل
-
 // 3. UI Settings
-define('PAGINATION_LIMIT', 50); // عدد العناصر الافتراضي في الجداول 
 define('APP_NAME', '7X Pharma Core');
 
 // 4. Roles
