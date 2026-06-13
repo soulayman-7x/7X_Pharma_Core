71749d506da4f9a6f645980c3de0454e9c020f5c
soulayman-7x <idbahasevenx@gmail.com>
2026-06-13 16:06:35 +0100

    refactor: update application name constant to '7X Pharma Nexus'

--- app/config/Constants.php
+++ app/config/Constants.php
@@ -2,7 +2,7 @@
 define('BASE_URL', 'http://localhost/7X_Pharma_Core/public');
 define('ROOT_DIR', dirname(__DIR__, 2));
 
-define('APP_NAME', '7X Pharma Core');
+define('APP_NAME', '7X Pharma Nexus');
 
 
 ?>
